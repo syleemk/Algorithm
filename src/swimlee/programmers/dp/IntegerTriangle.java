ce8c71914e71617d13238f519f4cf25e8ae8e53a
syleemk <syleemk@gmail.com>
2021-01-13 22:33:45 +0900

    [ADD] 프로그래머스 DP - 정수 삼각형

---
+++ src/swimlee/programmers/dp/IntegerTriangle.java
@@ -0,0 +1,35 @@
+package swimlee.programmers.dp;
+
+import java.util.Arrays;
+
+public class IntegerTriangle {
+
+    public static void main(String[] args) {
+        int[][] triangle = {{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}};
+
+        IntegerTriangle integerTriangle = new IntegerTriangle();
+        int solution = integerTriangle.solution(triangle);
+        System.out.println("solution = " + solution);
+    }
+
+    public int solution(int[][] triangle) {
+        int answer = 0;
+
+        int[][] dp = new int[triangle.length][triangle.length];
+        dp[0][0] = triangle[0][0];
+
+        for (int i = 1; i < triangle.length; i++) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + triangle[i][j];
+                } else if (j == triangle[i].length - 1) {
+                    dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
+                }
+            }
+        }
+
+        return Arrays.stream(dp[triangle.length-1]).max().getAsInt();
+    }
+}
