575c24c7ab7d48527b1f96295d941246f1701fc5
syleemk <syleemk@gmail.com>
2021-01-16 03:58:25 +0900

    [ADD] 프로그래머스 힙 - 디스크 컨트롤러

--- src/swimlee/programmers/heap/DiskController.java
+++ src/swimlee/programmers/heap/DiskController.java
@@ -1,33 +1,51 @@
 package swimlee.programmers.heap;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
+
+/**
+ * 해설 보고 품
+ */
 
 public class DiskController {
 
     public static void main(String[] args) {
-        int[][] jobs = {};
+        int[][] jobs = {{0, 3}, {1, 9}, {2, 6}};
         DiskController diskController = new DiskController();
         int solution = diskController.solution(jobs);
         System.out.println("solution = " + solution);
     }
 
     public int solution(int[][] jobs) {
-        int answer = 0;
+        int answer;
 
-        Queue<int[]> q = new LinkedList<>();
-        for (int[] job : jobs) {
-            q.add(job);
-        }
+        Arrays.sort(jobs, (a, b) -> {
+            if(a[0] == b[0]) return a[1] - b[1];
+            else return a[0] - b[0];
+        });
+        Queue<int[]> q = new LinkedList<>(Arrays.asList(jobs));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
 
         int time = 0;
-//        while (!q.isEmpty()) {
-//            if(q.peek())
-//        }
+        int totalWait = 0;
+        while (!q.isEmpty() || !pq.isEmpty()) {
+            //현재 작업 진행도중 오는 요청은 pq에 담는다
+            while (!q.isEmpty() && q.peek()[0] <= time) {
+                pq.offer(q.poll());
+            }
+            
+            if (!q.isEmpty() && pq.isEmpty()) {//대기 작업이 없는 경우 제일 우선순위 높은 작업 시간으로 업데이트
+                time = q.peek()[0];
+            } else {//대기 작업이 있는 경우, 가장 우선순위가 먼저인 작업 하나 수행
+                int[] now = pq.poll();
+                time += now[1];
+                totalWait += time - now[0];
+            }
 
+        }
 
+//        System.out.println(totalWait);
+//        System.out.println(jobs.length);
+        answer = totalWait / jobs.length;
         return answer;
     }
 }
