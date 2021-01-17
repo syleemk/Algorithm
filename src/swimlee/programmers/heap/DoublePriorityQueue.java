2bc82bf7836cde3a3b59370ab039082989839651
syleemk <syleemk@gmail.com>
2021-01-17 15:21:43 +0900

    [ADD] 백준 - 6603. 로또

---
+++ src/swimlee/programmers/heap/DoublePriorityQueue.java
@@ -0,0 +1,19 @@
+package swimlee.programmers.heap;
+
+import java.util.Arrays;
+
+public class DoublePriorityQueue {
+    public static void main(String[] args) {
+        String[] operatons = {};
+        DoublePriorityQueue doublePriorityQueue = new DoublePriorityQueue();
+        int[] solution = doublePriorityQueue.solution(operatons);
+        System.out.println("solution = " + Arrays.toString(solution));
+    }
+
+    public int[] solution(String[] operations) {
+        int[] answer = {};
+
+
+        return answer;
+    }
+}
