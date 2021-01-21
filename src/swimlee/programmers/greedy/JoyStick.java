668841decb56b1466b33a37218a94dfa864409f6
syleemk <syleemk@gmail.com>
2021-01-22 02:19:49 +0900

    [ADD] 프로그래머스 그리디 - 조이스틱

--- src/swimlee/programmers/greedy/JoyStick.java
+++ src/swimlee/programmers/greedy/JoyStick.java
@@ -1,29 +1,62 @@
 package swimlee.programmers.greedy;
 
+/**
+ * 문제 기준이 좀 이상한 듯
+ * 그리디임을 보장하기 힘듦
+ */
+
 public class JoyStick {
 
     public static void main(String[] args) {
         JoyStick joyStick = new JoyStick();
-        int solution = joyStick.solution("AAA");
+        int solution = joyStick.solution("ABAAAAAAAAABB");
         System.out.println("solution = " + solution);
     }
 
     public int solution(String name) {
         int answer = 0;
-        int curIdx = 0;
 
-        for (int i = 0; i < name.length(); i++) {
-            int move = 0;
-            if (name.charAt(i) != 'A') {
-                move = Math.min(i - curIdx, name.length() - (i - curIdx));
-                curIdx = i;
-                move += Math.min(name.charAt(i) - 'A', 26 - (name.charAt(i) - 'A'));
+        char[] arr = name.toCharArray();
+        int len = name.length();
+
+        int cur = 0;
+
+        while (true) {
+
+            boolean check = true;
+            for (char c : arr) {
+                if(c != 'A') {
+                    check = false;
+                    break;
+                }
             }
-            answer += move;
+            if(check) break;
+
+            int right = cur;
+            int left = cur;
+            int rightMv = 0;
+            int leftMv = 0;
+
+            while (arr[right] == 'A') {
+                if (++right >= len) {
+                    right = 0;
+                }
+                rightMv++;
+            }
+
+            while (arr[left] == 'A') {
+                if (--left < 0) {
+                    left = len - 1;
+                }
+                leftMv++;
+            }
+
+            cur = rightMv < leftMv ? right : left;
+            int move = Math.min(rightMv, leftMv);
+
+            answer += move + Math.min(arr[cur] - 'A', 'Z' + 1 - arr[cur]);
+            arr[cur] = 'A';
         }
-
-        boolean[] visited = new boolean[name.length()];
-
 
         return answer;
     }
