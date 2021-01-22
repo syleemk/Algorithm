bfb00bae2cd23e5a81c3e492f26c544e1d8d7efc
syleemk <syleemk@gmail.com>
2021-01-22 20:03:45 +0900

    [ADD] 백준 - 11657. 타임머신

--- src/swimlee/boj/MinRoute1753.java
+++ src/swimlee/boj/MinRoute1753.java
@@ -10,11 +10,11 @@ import java.util.*;
  * 완벽하게 이해못함
  */
 
-class Edge {
+class Node {
     public int vertex;
     public int weight;
 
-    public Edge(int vertex, int weight) {
+    public Node(int vertex, int weight) {
         this.vertex = vertex;
         this.weight = weight;
     }
@@ -32,7 +32,7 @@ public class MinRoute1753 {
 
         int[] dist = new int[V + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
-        List<Edge>[] list = new List[V+1];
+        List<Node>[] list = new List[V+1];
         for (int i = 0; i <= V; i++) {
             list[i] = new ArrayList<>();
         }
@@ -43,20 +43,20 @@ public class MinRoute1753 {
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
 
-            list[u].add(new Edge(v, w));
+            list[u].add(new Node(v, w));
         }
 
         dist[K] = 0;
-        PriorityQueue<Edge> pq = new PriorityQueue<>((a, b) -> a.weight - b.weight);
-        pq.add(new Edge(K, 0));
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> a.weight - b.weight);
+        pq.add(new Node(K, 0));
 
         while (!pq.isEmpty()) {
-            Edge now = pq.poll();
+            Node now = pq.poll();
             for (int i = 0; i < list[now.vertex].size(); i++) {
-                Edge next = list[now.vertex].get(i);
+                Node next = list[now.vertex].get(i);
                 if (dist[next.vertex] > dist[now.vertex] + next.weight) {
                     dist[next.vertex] = dist[now.vertex] + next.weight;
-                    pq.add(new Edge(next.vertex, dist[next.vertex]));
+                    pq.add(new Node(next.vertex, dist[next.vertex]));
                 }
             }
         }
