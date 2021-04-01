431995f2d7649f951c43ba6d3dc9f37c31c0c1ec
syleemk <syleemk@gmail.com>
2021-04-02 01:23:12 +0900

    [ADD] 프로그래머스 2021 카카오 블라인드 채용 - 신규아이디 추천

---
+++ src/swimlee/programmers/kakaoblind2021/NewIdRecommend.java
@@ -0,0 +1,46 @@
+package swimlee.programmers.kakaoblind2021;
+
+/**
+ * 정규표현식 기본 연습
+ */
+public class NewIdRecommend {
+
+    public static void main(String[] args) {
+        NewIdRecommend newIdRecommend = new NewIdRecommend();
+        String solution = newIdRecommend.solution("=.=");
+        System.out.println("solution = " + solution);
+    }
+
+    public String solution(String new_id) {
+        String answer = new_id;
+
+        // 1단계
+        answer = answer.toLowerCase();
+
+        // 2단계
+        answer = answer.replaceAll("[^a-z0-9-_.]", "");
+
+        // 3단계
+        answer = answer.replaceAll("[.]{2,}", ".");
+
+        // 4단계
+        answer = answer.replaceAll("^[.]","");
+        answer = answer.replaceAll("[.]$", "");
+
+        // 5단계
+        if(answer.isEmpty()) answer = "a";
+
+        // 6단계
+        if (answer.length() >= 16) {
+            answer = answer.substring(0, 15)
+                    .replaceAll("[.]$", "");
+        }
+
+        // 7단계
+        while (answer.length() <= 2) {
+            answer += answer.charAt(answer.length() - 1);
+        }
+
+        return answer;
+    }
+}
